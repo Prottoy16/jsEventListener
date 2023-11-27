4bde25496a7568164ed05724cdeb5cffb68296b9
Prottoy16 <prottoybiswas16@gmail.com>
2023-11-28 02:02:13 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var para=document.querySelector("p");
+
+var len=document.querySelectorAll(".button").length;
+
+for (i=0;i<len;i++){
+
+    document.querySelectorAll(".button")[i].addEventListener("click",function(){
+        var x= this.innerHTML;
+        para.innerHTML=x + " clicked";
+    
+    });
+
+}
+
+
